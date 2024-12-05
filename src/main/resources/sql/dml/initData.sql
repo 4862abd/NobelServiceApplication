80dd5110810c199a234c8ce415f0462114b8a9fe
bw <4862abd@gmail.com>
2024-12-06 01:47:31 +0900

    [SCHEMA] Fix typo

--- src/main/resources/sql/dml/initData.sql
+++ src/main/resources/sql/dml/initData.sql
@@ -3,7 +3,7 @@ insert into authority(authority_name) values ('ROLE_ADMIN');
 insert into authority(authority_name) values ('ROLE_GUEST');
 
 -- member
-insert into member(id, username, email, password, init_date, nickname) values (1, 'test', 'test@naver.com', 'test', now(), 'test');
+insert into member(id, username, email, password, init_date, nickname) values (1, 'test', 'test@naver.com', '$2a$10$fZkjGmY1ANCi6GYcFzFfGuiFbQ1RINI.uPYSda7TLIiuRbLJoq9fC', now(), 'test');
 
 -- member_authority
 insert into member_authority(member_id, authority_name) values(1, 'ROLE_ADMIN');
