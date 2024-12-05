2b1ee3bcd3bf8d5a52f4ab93bbd57c9f06fb2e77
bw <4862abd@gmail.com>
2024-12-05 16:56:07 +0900

    [SECURITY] ADD class for Spring Security

---
+++ src/main/java/com/example/novelserviceapplication/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.novelserviceapplication.config;
+
+import com.example.novelserviceapplication.domain.member.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+//                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer
+//                        .frameOptions(
+//                                HeadersConfigurer.FrameOptionsConfig::disable
+//                        ))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(
+                                new AntPathRequestMatcher("/")
+                                , new AntPathRequestMatcher("/css/**")
+                                , new AntPathRequestMatcher("/images/**")
+                                , new AntPathRequestMatcher("/js/**")
+                                , new AntPathRequestMatcher("/h2-console/**")
+                                , new AntPathRequestMatcher("/api/authorize")
+                        ).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/novel/**").getPattern())
+                        .permitAll()
+//                        .requestMatchers(new AntPathRequestMatcher("/api/**")).hasRole(Role.ADMIN.name())
+//                        .anyRequest().authenticated()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/"))
+                .build();
+    }
+}
